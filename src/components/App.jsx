a2e2816946f34c57556259e65c5e4efe5aaa19af
Oleh Protasevych <protasevicholeg@gmail.com>
2023-05-28 00:00:20 +0300

    Implement contacts persistence using localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,28 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    if (contacts) {
+      this.setState({ contacts: JSON.parse(contacts) });
+    } else {
+      this.setState({
+        contacts: [
+          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ],
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   handleSearchInputChange = ({ target: { name, value } }) => {
     this.setState({
       [name]: value,
